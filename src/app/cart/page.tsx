3138a95ca07ec5390aa5cff3ab1c3af8cc533503
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2023-12-03 22:41:01 +0700

    [update] integrated paypal

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -16,12 +16,12 @@ const Page = () => {
 
   const router = useRouter();
 
-  // const { mutate: createCheckoutSession, isLoading } =
-  //   trpc.payment.createSession.useMutation({
-  //     onSuccess: ({ url }) => {
-  //       if (url) router.push(url);
-  //     },
-  //   });
+  const { mutate: createCheckoutSession, isLoading } =
+    trpc.payment.createSession.useMutation({
+      onSuccess: ({ url }) => {
+        if (url) router.push(url);
+      },
+    });
 
   const productIds = items.map(({ product }) => product.id);
 
@@ -195,7 +195,7 @@ const Page = () => {
             </div>
 
             <div className="mt-6">
-              {/* <Button
+              <Button
                 disabled={items.length === 0 || isLoading}
                 onClick={() => createCheckoutSession({ productIds })}
                 className="w-full"
@@ -205,7 +205,7 @@ const Page = () => {
                   <Loader2 className="w-4 h-4 animate-spin mr-1.5" />
                 ) : null}
                 Checkout
-              </Button> */}
+              </Button>
             </div>
           </section>
         </div>
--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -5,7 +5,7 @@ import {
 import { PRODUCT_CATEGORIES } from "../../config";
 import { Access, CollectionConfig } from "payload/types";
 import { Product, User } from "../../payload-types";
-// import { stripe } from "../../lib/stripe";
+import { stripe } from "../../lib/stripe";
 
 const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
   const user = req.user;
@@ -91,36 +91,36 @@ export const Products: CollectionConfig = {
         if (args.operation === "create") {
           const data = args.data as Product;
 
-          // const createdProduct = await stripe.products.create({
-          //   name: data.name,
-          //   default_price_data: {
-          //     currency: "USD",
-          //     unit_amount: Math.round(data.price * 100),
-          //   },
-          // });
+          const createdProduct = await stripe.products.create({
+            name: data.name,
+            default_price_data: {
+              currency: "USD",
+              unit_amount: Math.round(data.price * 100),
+            },
+          });
 
-          // const updated: Product = {
-          //   ...data,
-          //   stripeId: createdProduct.id,
-          //   priceId: createdProduct.default_price as string,
-          // };
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
+          };
 
-          // return updated;
+          return updated;
         } else if (args.operation === "update") {
           const data = args.data as Product;
 
-          // const updatedProduct = await stripe.products.update(data.stripeId!, {
-          //   name: data.name,
-          //   default_price: data.priceId!,
-          // });
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!,
+          });
 
-          // const updated: Product = {
-          //   ...data,
-          //   stripeId: updatedProduct.id,
-          //   priceId: updatedProduct.default_price as string,
-          // };
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          };
 
-          // return updated;
+          return updated;
         }
       },
     ],
